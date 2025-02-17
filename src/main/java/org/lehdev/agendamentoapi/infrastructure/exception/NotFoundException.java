872613142c32ef97acfc7e6ea5fa1db8a3e97143
Leandro Amaral <lramaral02m@gmail.com>
2025-02-17 18:50:42 -0300

    Adicionado metodos de busca de agendamentos por ID

---
+++ src/main/java/org/lehdev/agendamentoapi/infrastructure/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package org.lehdev.agendamentoapi.infrastructure.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {super(message);}
+}
